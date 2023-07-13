cd9c626ee7e33feb67c312d6099dbc91b672f7bc
MuMuDosGrau <131720518+MuMuDosGrau@users.noreply.github.com>
2023-07-13 20:18:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const botaoPergunta = document.getElementById('botaoPergunta')
+
+Form.onSubmit = (evento) => {
+    evento = window.location.href = ("file:///C:/Users/joao_sturmer/Desktop/quiz/pergunta.html");
+}
